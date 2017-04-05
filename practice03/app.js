16664eb3fd55ca094f3c2db40447867dbb53f615
Dustin Vo <thanhduy2b@gmail.com>
2017-04-05 18:14:41 +0700

    Updated

--- practice03/app.js
+++ practice03/app.js
@@ -7,8 +7,8 @@ var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
-app.use('/assets', express.static(__dirname + 
-'/public'));
+app.use('/', express.static(__dirname + 
+'/public/src'));
 
 app.set('view engine', 'ejs');
 
