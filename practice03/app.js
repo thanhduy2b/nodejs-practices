790b8e3fc099e3fd43097df8e2eb02dbea21e480
Dustin Vo <thanhduy2b@gmail.com>
2017-04-05 14:36:17 +0700

    Updated

--- practice03/app.js
+++ practice03/app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var config = require('./config');
+var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
@@ -7,5 +11,9 @@ app.use('/assets', express.static(__dirname +
 '/public'));
 
 app.set('view engine', 'ejs');
+
+mongoose.connect(config.getDbConnectionString());
+setupController(app);
+apiController(app);
 
 app.listen(port);
